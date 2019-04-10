4328a77d71fa5f8b6968ec5dab08f7198393c047
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-10 15:28:45 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,25 @@
 class UsersController < ApplicationController
 
+  def logged_in?
+    # binding.pry
+    (session[:user_id] != "") ? true : false
+      # binding.pry
+  end
+
   get '/signup' do
+    binding.pry
+    if user.logged_in? == true
+    end
     erb :"/signup"
   end
 
   post '/signup' do
     user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-    if user.save
+    if (user.username != "") && (user.email != "") && (user.password != nil)
+      user.save
+      session[:user_id] = user.id
       redirect to "/tweets/tweets"
-    elsif user.email == ""
-      binding.pry
     else
       redirect "/signup"
     end
